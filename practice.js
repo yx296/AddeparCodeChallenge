30bebe5ba76392f04f792a2c028ef7f5448215fe
Yilin Xu <yx296@nyu.edu>
2015-12-28 23:44:02 -0800

    modular jQuery version

--- practice.js
+++ practice.js
@@ -1,49 +1,49 @@
 // Addepar asks you to build a website with 7 boxes shaped like a c, which should turn green on click, 
 // and then when all are clicked, should turn red in reverse clicking order
 
-document.addEventListener('DOMContentLoaded', function() {
-  var boxHolder = document.querySelector(".boxHolder");
-  var boxesNodeList = document.querySelectorAll(".box");
+$(document).ready(function() {
+
+  var boxHolder = $(".boxHolder");
+  var boxesNodeList = $(".box");
   var isUnwinding = false;
+  var timer;
   
-  boxHolder.addEventListener('click', changeGreen);
+  boxHolder.on('click', "div.box", changeGreen);
 
   var boxes = [];
 
   function changeGreen(e) { 
-    if (isUnwinding) {
-      return;
-    } 
+    if (isUnwinding) return;
 
-    if (e.target !== e.currentTarget) {
-      var box = e.target;
+    var $box = $(e.target);
 
-      if (!box.classList.contains('green')) { 
-        box.classList.remove('red');
-        box.classList.add('green');
-        boxes.push(e.target);
-      }
+    if (!$box.hasClass('green')) { 
+      $box.removeClass('red');
+      $box.addClass('green');
+      boxes.push($box);
     }
 
     if (boxes.length === boxesNodeList.length) { 
-      isUnwinding = true;
-      setTimeout(function() {
-        reverseTurnRed();
-      }, 500);
+      turnBoxesRed()
     }
-    e.stopPropagation();
   }
 
-  function reverseTurnRed() {
-    var timer = setInterval(function() {
-      var box = boxes.pop();
-      box.classList.remove('green');
-      box.classList.add('red');
-      if (!boxes.length) {
-        clearInterval(timer);
-        isUnwinding = false;
-      }
 
-    }, 300)
+  function turnBoxesRed() {
+    isUnwinding = true;
+    setTimeout(function() {
+      timer = setInterval(turnBoxRed, 300)
+    }, 500);
   }
+
+  function turnBoxRed () {
+    var box = boxes.pop();
+    box.removeClass('green');
+    box.addClass('red');
+    if (!boxes.length) {
+      clearInterval(timer);
+      isUnwinding = false;
+    }
+  }
+
 });
